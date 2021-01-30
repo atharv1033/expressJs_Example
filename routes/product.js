dfc1ef42b3399077383239c7935d967a6af52900
Atharv Andewar <atharvandewar1010@gmail.com>
2021-01-30 13:08:23 +0530

    First Commit

---
+++ routes/product.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.post('/add', function(req, res, next) {
+
+  res.send('')
+});
+
+router.get('/list', function(req, res, next) {
+
+    res.send('')
+});
+
+router.post('/add_to_cart', function(req, res, next) {
+
+    res.send('')
+});
+
+router.post('/list_cart', function(req, res, next) {
+    
+    res.send('')
+});
+
+module.exports = router;
